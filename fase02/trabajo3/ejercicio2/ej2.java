d1f003866b8a2682413734ca58622b0b62f8af15
joseticonasaure <148155761+joseticonasaure@users.noreply.github.com>
2023-12-23 18:15:18 -0500

    Create ej2.java

---
+++ fase02/trabajo3/ejercicio2/ej2.java
@@ -0,0 +1,7 @@
+public class ej2{
+  public static void main(String[] args) {
+        Cilindro cilindro = new Cilindro(3, 2, 5, 10);
+        double superficie = cilindro.superficie();
+        System.out.println("La superficie del cilindro es "+superficie);
+    }
+}
