ed387e9cc80310b25767ecf0cbf4a88f91c95730
joseticonasaure <148155761+joseticonasaure@users.noreply.github.com>
2023-12-23 22:39:20 -0500

    Update ej3.java

--- fase02/trabajo3/ejercicio3/ej3.java
+++ fase02/trabajo3/ejercicio3/ej3.java
@@ -1,1 +1,27 @@
--
+interface Barco {
+    void navegar();
+}
+interface Avion {
+    void volar();
+}
+
+// Clase Hidroavion que implementa las interfaces Barco y Avion
+class Hidroavion implements Barco, Avion{
+    @Override
+    public void navegar(){
+        System.out.println("El hidroavión navega por el agua.");
+    }
+
+    @Override
+    public void volar(){
+        System.out.println("El hidroavión despega y vuela.");
+    }
+}
+
+public class ej3{
+    public static void main(String[] args){
+        Hidroavion miHidroavion = new Hidroavion();
+        miHidroavion.navegar();
+        miHidroavion.volar();
+    }
+}
