680e8d990acf33e13b4aa48245e4272d40ba97a8
joseticonasaure <148155761+joseticonasaure@users.noreply.github.com>
2024-01-16 23:35:49 -0500

    Update Conculta.java

--- practica 02/Conculta.java
+++ practica 02/Conculta.java
@@ -3,19 +3,25 @@ package fp2_23b;
 import java.sql.*;
 
 public class Consulta {
-	public static Connection conec;
-	
-	Consulta() {
-	}
-	public static Connection getConnection() {
-		try {
-			if (conec == null) {
-				Class.forName("com.mysql.jdbc.Driver");
-				conec = DriverManager.getConnection("jbdc:mysql://localhost", "fp2", "1234");
-			}
-			return conec;
-		} catch (Exception e) {
-			System.out.println("conexion fallida");
-		}
-	}
+    private static Connection conec;
+
+    private Consulta() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            if (conec == null ) {
+                Class.forName("com.mysql.jdbc.Driver");
+                conec = DriverManager.getConnection("jdbc:mysql://localhost/tu_base_de_datos", "tu_usuario", "tu_contraseña");
+            }
+            return conec;
+        } catch (Exception e) {
+            System.out.println("Conexion fallida");
+            throw new RuntimeException("Error al establecer la conexión a la base de datos");
+        }
+    }
+
+    public static Consulta getInstance() {
+        return new Consulta();
+    }
 }
