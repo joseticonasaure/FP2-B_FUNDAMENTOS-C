8c6ecbc05e89d68c258c909ffb20ebb94e58303b
joseticonasaure <148155761+joseticonasaure@users.noreply.github.com>
2024-01-24 10:09:01 -0500

    Create Tablero.java

---
+++ p3/Tablero.java
@@ -0,0 +1,17 @@
+public class Tablero {
+	 public static void main(String[] args) {
+
+		     Runtime runtime = Runtime.getRuntime();
+	       long totalMemoria = runtime.totalMemory();
+	     
+	     //usando el patron de diseño MVC(modelo vista controlador)
+         Mapa mapa = new Mapa();
+         Vista vista = new Vista();
+	       Controler controller = new Controler(vista,mapa);
+         controller.mostrarMapa();
+	        
+         long memoriaLibre = runtime.freeMemory();
+	       long memoriaUsada = totalMemoria - memoriaLibre;
+         System.out.println("Memoria utilizada: " + memoriaUsada / (1024 * 1024) + "MB");
+    }
+}
